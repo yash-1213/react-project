b86c96168fc535b4e2c8062cdb9b5c8b3a3d4079
YASH GUPTA (IN) <YASH.GUPTA3@homecredit.co.in>
2026-03-05 13:00:00 +0530

    filtering of data using react hooks useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,32 @@
 import RestroCard from "./RestroCard";
+import { useState } from "react";
 import resObj from "../utils/mockData";
 
 const Body = () => {
+  // Local State Variable - super powerful variable
+  const [listOfRestroState, setListOfRestroState] = useState(resObj);
+
+  //   Another way to define state variable
+  //   const res = useState(resObj);
+  //   const listOfRestroState = res[0];
+  //   const setListOfRestroState = res[1];
+
   return (
     <div className="body-container">
-      <div className="search">Search</div>
+      <div className="filter-container">
+        <button
+          className="filter-container-btn"
+          onClick={() => {
+            const filteredList = listOfRestroState?.filter((restro) => restro.avgRating > 4);
+            console.log("filteredList :", filteredList);
+            setListOfRestroState(filteredList);
+          }}
+        >
+          Top Rated Restro
+        </button>
+      </div>
       <div className="restro-container">
-        {resObj.map((restro) => (
+        {listOfRestroState.map((restro) => (
           <RestroCard key={restro.id} resData={restro} />
         ))}
       </div>
