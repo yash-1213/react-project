e08c8efcaff45c0ae7e02e6862275ec0a0dbfd20
YASH GUPTA (IN) <YASH.GUPTA3@homecredit.co.in>
2026-03-07 20:41:41 +0530

    added tailwind css library

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,27 +5,27 @@ import { Link } from "react-router";
 const Header = () => {
   const [btnName, setBtnName] = useState("login");
   return (
-    <div className="header">
+    <div className="flex justify-between items-center bg-pink-100 mb-2">
       <div className="logo-container">
-        <img className="logo-container-img" src={LOGO_URL} />
+        <img className="w-24" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>
+      <div className="">
+        <ul className="flex p-4">
+          <li className="ml-2">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="ml-2">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="ml-2">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="ml-2">
             <Link to="/grocery">Grocery</Link>
           </li>
           <li>Cart</li>
           <button
-            className="login"
+            className="ml-2 p-2 hover:bg-green-100 bg-gray-100 rounded-[3px]"
             onClick={() => {
               setBtnName(btnName == "login" ? "logout" : "login");
             }}
